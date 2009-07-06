e7221b2c63ebcb95700013a4a1cd2b5e58c03990
Adeel <adeel2@umbc.edu>
2009-07-05 23:29:40 -0400

    htmltidy config

--- htmltidy.py
+++ htmltidy.py
@@ -5,7 +5,7 @@ try:
 
   def tidy(html):
     html, errors = tidylib.tidy_document(html, options={'force-output': True,
-      'drop-empty-paras': True, 'output-xhtml': True, 'tidy-mark': False})
+      'output-xhtml': True, 'tidy-mark': False})
     return html
   
 except ImportError:
